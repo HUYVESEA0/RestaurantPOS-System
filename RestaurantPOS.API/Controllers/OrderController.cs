fa10a61dd3c56411c4f02530f390ac675eeb53c5
HUYVIESEA <161063550+HUYVESEA0@users.noreply.github.com>
2025-10-21 14:06:14 +0700

    Add OrderController.cs

---
+++ RestaurantPOS.API/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+// Controller for managing orders
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantPOS.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        // Actions for managing orders will go here
+    }
+}
