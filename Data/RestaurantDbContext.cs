be1f24634d5a1caa413f04c60d4029dd80612f21
HUYVIESEA <161063550+HUYVESEA0@users.noreply.github.com>
2025-10-22 07:35:01 +0700

    Create Database Context for Restaurant POS System

---
+++ Data/RestaurantDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantPOS_System.Data
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public RestaurantDbContext(DbContextOptions<RestaurantDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MenuItem> MenuItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
